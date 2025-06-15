57e25195bd544c17b564dd7170792b252aea96a9
Rahman <rahmansahinler1@gmail.com>
2025-06-15 15:07:46 +0300

    fix: update waitlist and wishlist tables to enforce NOT NULL constraint on user_email

--- app/db/sql/initialize_database.sql
+++ app/db/sql/initialize_database.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS waitlist_info (
-    user_email VARCHAR(100) PRIMARY KEY,
+    user_email VARCHAR(100) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS wishlist_info (
-    user_email VARCHAR(100) PRIMARY KEY,
+    user_email VARCHAR(100) NOT NULL,
     user_name VARCHAR(50),
     user_surname VARCHAR(50),
     user_message VARCHAR(255),
