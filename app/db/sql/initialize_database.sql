389644e36fc63b8361a9d01393c2707a0da8fb1d
Rahman <rahmansahinler1@gmail.com>
2025-06-20 21:46:03 +0300

    feat: redesign database schema to include user_info, brand_info, and overview_data tables

--- app/db/sql/initialize_database.sql
+++ app/db/sql/initialize_database.sql
@@ -1,12 +1,28 @@
-CREATE TABLE IF NOT EXISTS waitlist_info (
-    user_email VARCHAR(100) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE IF NOT EXISTS user_info (
+    user_id UUID PRIMARY KEY,
+    user_name VARCHAR(50) NOT NULL,
+    user_surname VARCHAR(50) NOT NULL,
+    user_email VARCHAR(100) UNIQUE NOT NULL,
+    user_type VARCHAR(20) NOT NULL,
+    user_created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS wishlist_info (
-    user_email VARCHAR(100) NOT NULL,
-    user_name VARCHAR(50),
-    user_surname VARCHAR(50),
-    user_message VARCHAR(255),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE IF NOT EXISTS brand_info (
+    user_id UUID NOT NULL,
+    brand_id UUID PRIMARY KEY,
+    brand_name VARCHAR(50) NOT NULL,
+    domain VARCHAR(100) NOT NULL,
+    brand_niches TEXT[] NOT NULL,
+    last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES user_info(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS overview_data (
+    overview_id UUID PRIMARY KEY,
+    user_id UUID NOT NULL,
+    brand_id UUID NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    ai_seo_score INTEGER CHECK (ai_seo_score >= 0 AND ai_seo_score <= 100),
+    FOREIGN KEY (user_id) REFERENCES user_info(user_id),
+    FOREIGN KEY (brand_id) REFERENCES brand_info(brand_id)
 );
