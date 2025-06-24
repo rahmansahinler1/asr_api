ef1a09f4bad9531c8431c99b88f8cff94131f673
Rahman <rahmansahinler1@gmail.com>
2025-06-24 11:31:45 +0300

    fix: additional ports for development environment

--- app/main.py
+++ app/main.py
@@ -12,7 +12,11 @@ app.add_middleware(
         "http://localhost:3000", 
         "http://127.0.0.1:3000",
         "http://localhost:5500",
-        "http://127.0.0.1:5500"
+        "http://127.0.0.1:5500",
+        "http://localhost:5501",
+        "http://127.0.0.1:5501",
+        "http://localhost:5502",
+        "http://127.0.0.1:5502"
     ],
     allow_credentials=True,
     allow_methods=["*"],
